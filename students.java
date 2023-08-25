bc4355ad56b3ae26e7b0b18cbe2b2cf003648114
Chris Maynard D. Ampon <65159001+Hearteu@users.noreply.github.com>
2023-08-24 23:23:59 -0700

    Update students.java

--- students.java
+++ students.java
@@ -1,6 +1,6 @@
 private void btn_studentsActionPerformed(java.awt.event.ActionEvent evt) {                                             
         System.out.print("Save");
-        String saveDB = "insert into students values(1,'aa','bb','cc','dd','ee')";
+        String saveDB = "insert into students values("+txt_studID.getText()+",'"+txt_studName.getText()+"','"+txt_studAdd.getText()+"','"+txt_studCourse.getText()+"','"+txt_studGender.getText()+"','"+txt_studYear.getText()+"')";
         try{
           MyDBConn a = new MyDBConn();    
           a.st.executeUpdate(saveDB);
