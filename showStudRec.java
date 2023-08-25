61c8888329a3befb9ad55d00f09730c29c374fac
Chris Maynard D. Ampon <65159001+Hearteu@users.noreply.github.com>
2023-08-24 23:41:33 -0700

    Create showStudRec.java

---
+++ showStudRec.java
@@ -0,0 +1,23 @@
+private void ShowStudRec(){
+        DefaultTableModel tblmodel = (DefaultTableModel) table_students.getModel();
+        tblmodel.setRowCount(0);
+        MyDBConn a = new MyDBConn();
+        try {
+            String query = "";
+            a.rs = a.st.executeQuery(query);
+            while (a.rs.next())
+                {
+                    String id = a.rs.getString("StudID");
+                    String name = a.rs.getString("Studname");
+                    String addr = a.rs.getString("StudAdd");
+                    String course = a.rs.getString("StudCourse");
+                    String gender = a.rs.getString("StudGender");
+                    String yearlevel = a.rs.getString("StudYL");
+                    String[] item = {id,name,addr, course,gender,yearlevel};
+                    tblmodel.addRow(item);
+                }
+        } catch(Exception ex)
+        {
+            System.out.println(ex);
+        }
+    }
