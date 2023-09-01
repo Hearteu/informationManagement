5679bbd2e227ffdff14909a4e86316ff6411e812
Chris Maynard D. Ampon <65159001+Hearteu@users.noreply.github.com>
2023-08-31 22:51:09 -0700

    Update showStudRec.java

--- showStudRec.java
+++ showStudRec.java
@@ -48,3 +48,27 @@ private void ShowTeachRec(){
             System.out.println(ex);
         }
     }
+
+// subjects
+ private void ShowSubjRec(){
+        DefaultTableModel tblmodel = (DefaultTableModel) table_subjects.getModel();
+        tblmodel.setRowCount(0);
+        MyDBConn a = new MyDBConn();
+        try {
+            String query = "select * from Subjects";
+            a.rs = a.st.executeQuery(query);
+            while (a.rs.next())
+                {
+                    String id = a.rs.getString("subjID");
+                    String name = a.rs.getString("subjCode");
+                    String addr = a.rs.getString("subjDesc");
+                    String course = a.rs.getString("subjUnits");
+                    String gender = a.rs.getString("subjSched");
+                    String[] item = {id,name,addr, course,gender};
+                    tblmodel.addRow(item);
+                }
+        } catch(Exception ex)
+        {
+            System.out.println(ex);
+        }
+    }
