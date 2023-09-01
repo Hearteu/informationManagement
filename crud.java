14f9582672ba95a6d41c83ce1e7ac54d30d9c0d7
Chris Maynard D. Ampon <65159001+Hearteu@users.noreply.github.com>
2023-08-31 22:50:02 -0700

    Update crud.java

--- crud.java
+++ crud.java
@@ -58,3 +58,32 @@ System.out.print("Update");
             System.out.print("Unable to Save!!");
         }
         ShowTeachRec();
+
+// subjects
+ System.out.print("Save");
+        String saveDB = "insert into Subjects values('"+txt_subjID.getText()+"','"+txt_subjCode.getText()+"','"+txt_subjDesc.getText()+"','"+txt_subjUnits.getText()+"','"+txt_subjSched.getText()+"')";
+        try{
+          MyDBConn a = new MyDBConn();    
+          a.st.executeUpdate(saveDB);
+        }catch(Exception ex){
+            System.out.print("Unable to Save!!");
+        }
+        ShowSubjRec();
+System.out.print("Update");
+        String updateDB = "update Subjects set subjCode='"+txt_subjCode.getText()+"',subjDesc='"+txt_subjDesc.getText()+"',subjUnits='"+txt_subjUnits.getText()+"',subjSched='"+txt_subjSched.getText()+"' where subjID=" + txt_subjID.getText();
+        try{
+          MyDBConn a = new MyDBConn();    
+          a.st.executeUpdate(updateDB);
+        }catch(Exception ex){
+            System.out.print("Unable to Update!!" + ex);
+        }
+        ShowSubjRec();
+System.out.print("Delete");
+        String deleteDB = "delete from Subjects where subjID=" + txt_subjID.getText();
+        try{
+          MyDBConn a = new MyDBConn();    
+          a.st.executeUpdate(deleteDB);
+        }catch(Exception ex){
+            System.out.print("Unable to Delete!!");
+        }
+        ShowSubjRec();
