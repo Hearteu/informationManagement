ba30997957af9e141edfffddeb5b243a624493a2
Chris Maynard D. Ampon <65159001+Hearteu@users.noreply.github.com>
2023-09-14 23:37:48 -0700

    Update exer2.java

--- exer2.java
+++ exer2.java
@@ -1,6 +1,8 @@
 // Students
+
 // global var 
 public String studid, subjID;
+
 // Enroll
  System.out.print("Enroll");
         String saveDB = "insert into enroll(studID,subjID) values("+studid+","+subjID+")";
@@ -27,6 +29,31 @@ System.out.print("Drop");
             System.out.print("Unable to Drop!!");
         }
         ShowEnrolledSubjRec();
+
+// showSubjRec
+private void ShowSubjRec(){
+        DefaultTableModel tblmodel = (DefaultTableModel) table_studsubjects.getModel();
+        tblmodel.setRowCount(0);
+        MyDBConn a = new MyDBConn();
+        try {
+            String query = "select * from Subjects";
+            a.rs = a.st.executeQuery(query);
+            while (a.rs.next())
+                {
+                    String id = a.rs.getString("subjID");
+                    String name = a.rs.getString("subjCode");
+                    String addr = a.rs.getString("subjDesc");
+                    String course = a.rs.getString("subjUnits");
+                    String gender = a.rs.getString("subjSched");
+                    String[] item = {id,name,addr, course,gender};
+                    tblmodel.addRow(item);
+                }
+        } catch(Exception ex)
+        {
+            System.out.println(ex);
+        }
+    }
+
 // show EnrolleSUbj();
 private void ShowEnrolledSubjRec(){
         DefaultTableModel tblmodel = (DefaultTableModel) table_studSubj.getModel();
