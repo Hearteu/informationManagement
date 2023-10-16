18e6aef779138b3e1bb5f9c3d0db402828e98fb8
Chris Maynard D. Ampon <65159001+Hearteu@users.noreply.github.com>
2023-10-16 01:37:01 -0700

    Update exer4.java

--- exer4.java
+++ exer4.java
@@ -1,14 +1,53 @@
 // LOGIN
-
+        MyDBConn db = new MyDBConn();
+        db.username = txt_username.getText();
+        db.password = txt_password.getText();
+        cbox_sy.removeAllItems();
+        ShowDB();
+    private void ShowDB(){
+        MyDBConn a = new MyDBConn();
+        try {
+            String query = "Show databases";
+            a.rs = a.st.executeQuery(query);
+            while (a.rs.next())
+                {
+                    String db = a.rs.getString("Database");
+                    
+                    if(!db.equals("information_schema")){
+                        cbox_sy.addItem(db);
+                    }
+                }
+        } catch(Exception ex)
+        {
+            System.out.println(ex);
+        }
+    }
 // MyDBConn Update
-public MyDBConn(){
+public class MyDBConn {
+    public Connection con;
+    public Statement st;
+    public ResultSet rs;
+    
+    String db, username, password;
+    
+    
+    public MyDBConn(){
+        username = "root";
+        password = "root";
+        
+            db="information_schema";
+        
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://10.4.40.70:3306/progexer1?zeroDateTimeBehavior=convertToNull","root","root");
+            con = DriverManager.getConnection("jdbc:mysql://10.4.40.70:3306/" + db + "?zeroDateTimeBehavior=convertToNull",username,password); 
             st = con.createStatement();  
             System.out.println("Connected");
         }catch (Exception ex) {
           System.out.print(ex);
         }
     }
+}
 
+//students 
+String createuser = "create user 'username'@localhost identified by 'passowrd'";
+String grant = "grant select on *.* to 'user'@localhost identified by 'password'";
