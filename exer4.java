2f11a9ba0084d3cea2fe86fb92d707ac4c73add8
Chris Maynard D. Ampon <65159001+Hearteu@users.noreply.github.com>
2023-10-16 00:47:22 -0700

    Update exer4.java

--- exer4.java
+++ exer4.java
@@ -1,2 +1,14 @@
-LOGIN
+// LOGIN
+
+mydbupdate
+public MyDBConn(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://10.4.40.70:3306/progexer1?zeroDateTimeBehavior=convertToNull","root","root");
+            st = con.createStatement();  
+            System.out.println("Connected");
+        }catch (Exception ex) {
+          System.out.print(ex);
+        }
+    }
 
