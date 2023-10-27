70eccc087bd873638237903ff4d85c3e5cbcbec1
Chris Maynard D. Ampon <65159001+Hearteu@users.noreply.github.com>
2023-10-26 23:49:40 -0700

    Update exer4.java

--- exer4.java
+++ exer4.java
@@ -61,8 +61,25 @@ public class MyDBConn {
 }
 //teachers
 String user = "T" + txt_teacherName.getText();
-String createuser = "create user '"+user+"'@'10.4.40.174' identified by '"+txt_studName.getText()+txt_studID.getText()+"'";
-String grant = "grant select on "+currentdb+".* to '"+txt_teacherName.getText()+"'@'10.4.40.174'";
+String createuser = "create user '"+user+"'@'10.4.40.174' identified by '"+txt_teacherName.getText()+txt_teacherID.getText()+"'";
+String grant = "grant select,insert,update on "+currentdb+".* to '"+user+"'@'10.4.40.174'";
+String saveDB = "insert into teachers values("+txt_teacherID.getText()+",'"+txt_teacherName.getText()+"','"+txt_department.getText()+"','"+txt_teacherAdd.getText()+"','"+txt_teacherContact.getText()+"','"+txt_teacherStatus.getText()+"')";
+try{
+        a.st.executeUpdate(saveDB);
+        System.out.println("SAVED");
+        checkuser();
+        System.out.println("checked");
+        if (!aa.equals("exists")){
+                a.st.executeUpdate(createuser);
+                System.out.println("CREATED USER");
+          }
+          a.st.executeUpdate(grant);
+          System.out.println("Granted to: " + currentdb);
+        }catch(Exception ex){
+            System.out.print("Unable to Save!!" + ex);
+        }
+        ShowTeachRec();
+    }                       
 //students 
 String user = "S" + txt_teacherName.getText();
 String createuser = "create user '"+txt_studName.getText()+"'@'10.4.40.174' identified by '"+txt_studName.getText()+"s"+txt_studID.getText()+"'";
@@ -86,3 +103,22 @@ String grant = "grant select on "+db+".* to '"+txt_studName.getText()+"'@'10.4.4
             System.out.print("Unable to Save!!" + ex);
         }
         ShowStudRec();
+//checkuser
+ public void checkuser(){
+        try {
+            String count = "select count(*) as a from mysql.user where user='T"+txt_teacherName.getText()+"'";
+            a.rs = a.st.executeQuery(count);
+            while (a.rs.next()){
+                String users = a.rs.getString("a");
+                if (users.equals("1")){
+                    aa = "exists";
+                }
+                else{
+                    aa ="";
+                }
+            }
+        }catch (Exception ex){
+            System.out.println(ex);
+        }
+        
+    }
