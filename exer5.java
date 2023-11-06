64d45b34dfe660f93d70c99f583b20a2c7ac94ea
Chris Maynard D. Ampon <65159001+Hearteu@users.noreply.github.com>
2023-11-05 23:41:40 -0800

    Update exer5.java

--- exer5.java
+++ exer5.java
@@ -1,1 +1,5 @@
+select * from customers inner join orders on customers.cid = orders.cid;
+// compare cid of orders and cid of customers
+
+select * from customers right outer join orders on customers.cid = orders.cid;
 
